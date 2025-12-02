bf26590a6e9b013b7311649dbd972c3d01d1c797
shohag80 <mdshohaghossain8080@gmail.com>
2025-12-02 18:52:09 +0600

    web header

--- resources/views/Frontend/Partials/Header.blade.php
+++ resources/views/Frontend/Partials/Header.blade.php
@@ -132,10 +132,6 @@
           </form>
         </div>
         <div class="col-md-1 col-xxl-3 d-none d-lg-block">
-          <!-- Button trigger modal -->
-          <button type="button" class="btn  btn-outline-gray-800 text-muted" data-bs-toggle="modal" data-bs-target="#locationModal">
-            Location
-          </button>
         </div>
         <div class="col-md-3 col-xxl-2 text-center d-none d-lg-block">
           <div class="list-inline">
@@ -508,59 +504,3 @@
 </div>
 </div>
 @endauth
-
-
-<!-- Modal -->
-<div class="modal fade" id="locationModal" tabindex="-1" aria-labelledby="locationModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-sm modal-dialog-centered">
-    <div class="modal-content">
-
-      <div class="modal-body p-6">
-        <div class="d-flex justify-content-between align-items-start ">
-          <div>
-            <h5 class="mb-1" id="locationModalLabel">Choose your Delivery Location</h5>
-            <p class="mb-0 small">Enter your address and we will specify the offer you area. </p>
-          </div>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="my-5">
-          <input type="search" class="form-control" placeholder="Search your area">
-        </div>
-        <div class="d-flex justify-content-between align-items-center mb-2">
-          <h6 class="mb-0">Select Location</h6>
-          <a href="#" class="btn btn-outline-gray-400 text-muted btn-sm">Clear All</a>
-
-
-        </div>
-        <div>
-          <div data-simplebar style="height:300px;">
-            <div class="list-group list-group-flush">
-
-              <a href="#" class="list-group-item d-flex justify-content-between align-items-center px-2 py-3 list-group-item-action">
-                <span>Uttara</span><span>Min:$29</span></a>
-              <a href="#" class="list-group-item d-flex justify-content-between align-items-center px-2 py-3 list-group-item-action">
-                <span>Uttar Khan</span><span>Min:$80</span></a>
-              <a href="#" class="list-group-item d-flex justify-content-between align-items-center px-2 py-3 list-group-item-action">
-                <span>Uttara East</span><span>Min:$90</span></a>
-              <a href="#" class="list-group-item d-flex justify-content-between align-items-center px-2 py-3 list-group-item-action">
-                <span>Uttara West</span><span>Min:$90</span></a>
-              <a href="#" class="list-group-item d-flex justify-content-between align-items-center px-2 py-3 list-group-item-action">
-                <span>Badda</span><span>Min:$30</span></a>
-              <a href="#" class="list-group-item d-flex justify-content-between align-items-center px-2 py-3 list-group-item-action">
-                <span>Banani</span><span>Min:$50</span></a>
-              <a href="#" class="list-group-item d-flex justify-content-between align-items-center px-2 py-3 list-group-item-action">
-                <span>Turag</span><span>Min:$29</span></a>
-              <a href="#" class="list-group-item d-flex justify-content-between align-items-center px-2 py-3 list-group-item-action">
-                <span>Bimanbandar</span><span>Min:$80</span></a>
-              <a href="#" class="list-group-item d-flex justify-content-between align-items-center px-2 py-3 list-group-item-action">
-                <span>Cantonment</span><span>Min:$90</span></a>
-              <a href="#" class="list-group-item d-flex justify-content-between align-items-center px-2 py-3 list-group-item-action">
-                <span>Chalkbazar</span><span>Min:$50</span></a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </div>
-</div>
