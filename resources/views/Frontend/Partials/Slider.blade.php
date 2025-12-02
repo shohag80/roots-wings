96c05ee8026b15aae9056dab72395176114f1b62
shohag80 <mdshohaghossain8080@gmail.com>
2025-12-02 11:01:27 +0600

    slider

--- resources/views/Frontend/Partials/Slider.blade.php
+++ resources/views/Frontend/Partials/Slider.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="hero-slider">
       @foreach($slider as $item)
-      <div style="background: url('{{url('public/uploads/slider_img').'/'.$item->photo}}.') no-repeat; background-size: cover; border-radius: .5rem; background-position: center;">
+      <div style="background: url('{{url('public/uploads/slider_img').'/'.$item->photo}}') no-repeat; background-size: cover; border-radius: .5rem; background-position: center;">
         <div class="ps-lg-12 py-lg-16 col-xxl-5 col-md-7 py-14 px-8 text-xs-center">
           <span class="badge text-bg-warning">{{$item->sub_title}}</span>
           <h2 class="text-white display-5 fw-bold mt-4">{{$item->title}}</h2>
