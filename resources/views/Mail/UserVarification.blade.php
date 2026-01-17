397441219880bf34329649b59c00da2cde5e6312
MD. SHOHAG HOSSAIN <90141002+shohag80@users.noreply.github.com>
2026-01-17 11:43:50 +0600

    Update UserVarification.blade.php

--- resources/views/Mail/UserVarification.blade.php
+++ resources/views/Mail/UserVarification.blade.php
@@ -5,19 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Online Shoping</title>
+    <title>ROOTS & WINGS</title>
 </head>
 
 <body>
     <pre>
         Dear {{ $data->name }},
 
-        Assalamu-Alaikum! Congratulation. Your Account was created Successfully. Click for Active your Account.
+        Assalamu-Alaikum! Congratulation. Your Account was created Successfully. 
+        Click for Active your Account.
 
-        <a href="{{ route('active_user_account', $data->remember_token) }}">Activate</a>
+        <a href="{{ route('active_user_account', $data->remember_token) }}"><button style="padding: 3px 5px; background-color: green; border-radius: 5%; color: white;">Activate</button></a>
 
         Thanks,
-        Online Shop Team
+        Roots & Wings Team
     </pre>
 </body>
 
